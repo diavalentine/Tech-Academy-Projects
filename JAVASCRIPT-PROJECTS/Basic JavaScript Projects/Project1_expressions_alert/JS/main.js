af0be3eed1f15ad3cb8583e753c4d8bf70bab2fe
diavalentine <diavalentine.biz@gmail.com>
2026-09-15 17:14:07 -0700

    first JS assignment

---
+++ JAVASCRIPT-PROJECTS/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+
+var day = "bonjour", morning = "bon matin", evening = "bonsoir", night = "bonne nuit"; //this is french
+document.write(day); 
+window.alert(morning); //this creates a pop up alert
+
+sent1 = "This is the beginning of the string", sent2 = " and this is the end of the string"; //defining 2 variables
+document.write(sent1 + sent2); //writing out the concatenated string
