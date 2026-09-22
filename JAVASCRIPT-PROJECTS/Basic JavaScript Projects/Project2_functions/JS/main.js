bc844293f63e6ccf6251cdd3631ab40b51afd91e
diavalentine <diavalentine.biz@gmail.com>
2026-09-22 13:25:17 -0700

    added comments

--- JAVASCRIPT-PROJECTS/Basic JavaScript Projects/Project2_functions/JS/main.js
+++ JAVASCRIPT-PROJECTS/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,6 @@
 
 function myFunction(sentEnd){
-    result = "The rest of this sentence is...";
-    result += sentEnd;
-    document.getElementById("sentence").innerHTML = result;
+    result = "The rest of this sentence is..."; 
+    result += sentEnd; //sentEnd is parameter to be sent argument
+    document.getElementById("sentence").innerHTML = result; //innerHTML allows me to change the text of p element
 }
